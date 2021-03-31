e6bff1f45c28f681fae709688365225d15b4f1de
ahsouza <annibalhsouza@gmail.com>
2021-03-30 23:36:25 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,25 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Acccount Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      username: 'any_name',
+      email: 'any_email',
+      password: 'any_password',
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.username).toBe('any_username')
+    expect(account.email).toBe('any_email')
+    expect(account.password).toBe('any_password')
+  })
+})
